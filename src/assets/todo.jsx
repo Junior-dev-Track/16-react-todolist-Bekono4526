ee7618274023ede10a30d48c86f4473bcccd2940
Bekono4526 <bekonorosy0@gmail.com>
2024-04-22 15:56:26 +0200

    todo

---
+++ src/assets/todo.jsx
@@ -0,0 +1,13 @@
+function todo() {
+  return (
+    <>
+      <div>
+        <form>
+          <input type="text" name="Type a new todo" />
+        </form>
+        <button>Add a new todo</button>
+        <hr />
+      </div>
+    </>
+  );
+}
